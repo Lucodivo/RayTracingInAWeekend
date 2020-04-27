756aa1aa1200b025edf4785fd862f3cff8ad9398
Connor Haskins <ConnorAHaskins@gmail.com>
2020-04-27 02:30:46 -0700

    Final render

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Following along to [Peter Shirley's Ray Tracing in One Weekend](https://www.realtimerendering.com/raytracing/Ray%20Tracing%20in%20a%20Weekend.pdf) but it's a weekday 😈
+
+Final result:
+![alt tag](https://raw.githubusercontent.com/Lucodivo/RayTracingInAWeekend/master/example.png)
--- src/main.cpp
+++ src/main.cpp
@@ -35,25 +35,53 @@ Vec3 color(const Ray& ray, const Hittable* world, int iteration) {
   return missColor(ray);
 }
 
+Hittable *randomScene() {
+  int n = 500;
+  Hittable **list = new Hittable*[n + 1];
+  list[0] = new Sphere(Vec3(0.0, -1000.0, 0.0), 1000.0, new Lambertian(Vec3(0.5, 0.5, 0.5)));
+  int i = 1;
+  for(int a = -11; a < 11; ++a) {
+    for(int b = -11; b < 11; ++b) {
+      float materialProb = randomFraction();
+      Vec3 center(a + 0.9 * randomFraction(), 0.2, b + 0.9 * randomFraction());
+      if((center - Vec3(1.0, 0.2, 0.0)).length() > 0.9) {
+        if(materialProb < 0.8) { // Lambertian
+          list[i++] = new Sphere(center, 0.2, new Lambertian(Vec3(randomFraction() * randomFraction(), randomFraction() * randomFraction(), randomFraction() * randomFraction())));
+        } else if(materialProb < 0.95) {
+          list[i++] = new Sphere(center, 0.2, new Metal(Vec3(0.5 * (1 + randomFraction()),0.5 * (1 + randomFraction()), 0.5 * (1 + randomFraction())), 0.5 * randomFraction()));
+        } else {
+          list[i++] = new Sphere(center, 0.2, new Dielectric(1.5));
+        }
+      }
+    }
+  }
+
+  list[i++] = new Sphere(Vec3(0.0, 1.0, 0.0), 1.0, new Dielectric(1.5));
+  list[i++] = new Sphere(Vec3(-4.0, 1.0, 0.0), 1.0, new Lambertian(Vec3(0.4, 0.2, 0.1)));
+  list[i++] = new Sphere(Vec3(4.0, 1.0, 0.0), 1.0, new Metal(Vec3(0.7, 0.6, 0.5), 0.0));
+
+  return new HittableList(list, i);
+}
+
 int main()
 {
   // ppm header: P3 means colors are in ascii, dimens are pixelWidth by pixelHeight, each pixel ranges from 0-255
   std::cout << "P3\n" << pixelWidth << " " << pixelHeight << "\n255\n";
 
-  Vec3 lookFrom(3.0, 3.0, 2.0);
-  Vec3 lookAt(0.0, 0.0, -1.0);
+  Vec3 lookFrom(13.0, 2.0, 3.0);
+  Vec3 lookAt(0.0, 0.0, 0.0);
   float focusDist = (lookAt - lookFrom).length();
-  float aperture = 2.0;
+  float aperture = 0.1;
   Camera camera(lookFrom, lookAt, Vec3(0.0, 1.0, 0.0), 20.0, (float)pixelWidth / (float)pixelHeight, aperture, focusDist);
 
-  Hittable *spheres[] = {
-          new Sphere(Vec3(0.0, 0.0, -1.0), 0.5, new Lambertian(Vec3(0.1, 0.2, 0.5))),
-          new Sphere(Vec3(0.0, -100.5, -1.0), 100, new Lambertian(Vec3(0.8, 0.8, 0.0))),
-          new Sphere(Vec3(1.0, 0, -1.0), 0.5, new Metal(Vec3(0.8, 0.6, 0.2), 0.3)),
-          new Sphere(Vec3(-1.0, 0.0, -1.0), 0.5, new Dielectric(1.5)),
-          new Sphere(Vec3(-1.0, 0.0, -1.0), -0.45, new Dielectric(1.5))
-  };
-  Hittable *world = new HittableList(spheres, ArrayCount(spheres));
+//  Hittable *spheres[] = {
+//          new Sphere(Vec3(0.0, -1000.0, 0.0), 1000.0, new Lambertian(Vec3(0.5, 0.5, 0.5))),
+//          new Sphere(Vec3(0.0, 1.0, 0.0), 1.0, new Dielectric(1.5)),
+//          new Sphere(Vec3(-4.0, 1.0, 0.0), 1.0, new Lambertian(Vec3(0.4, 0.2, 0.1))),
+//          new Sphere(Vec3(4.0, 1.0, 0.0), 1.0, new Metal(Vec3(0.7, 0.6, 0.5), 0.0))
+//  };
+//  Hittable *world = new HittableList(spheres, ArrayCount(spheres));
+  Hittable *world = randomScene();
   for(int pixelY = pixelHeight - 1; pixelY >= 0; --pixelY) {
     for(int pixelX = 0; pixelX < pixelWidth; ++pixelX ) {
       Vec3 col(0.0, 0.0, 0.0);
